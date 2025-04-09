63dd9da99013a3b538a97237f03b38f684253754
BrivaHamisi <brivahamisi@gmail.com>
2025-04-09 23:45:58 +0300

    Django and tailwind css

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,48 @@
+# from django.contrib import admin
+# # from .models import Question, Choice
+# #
+# # class QuestionAdmin(admin.ModelAdmin):
+# #     fieldsets = [
+# #         (None, {"fields": ["question_text"]}),
+# #         ("Date information", {"fields": ["pub_date"]}),
+# #     ]
+# #
+# #
+# # admin.site.register(Question, QuestionAdmin)
+# #
+# # admin.site.register(Choice)
+
+
 from django.contrib import admin
-from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+from .models import Choice, Question
 
+
+class ChoiceInline(admin.StackedInline):
+# class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    # @admin.display(
+    #     boolean=True,
+    #     ordering="pub_date",
+    #     description="Published recently?",
+    # )
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_filter = ["pub_date"]
+
+
+admin.site.register(Question, QuestionAdmin)
